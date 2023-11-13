778ea092ea3c7cf023da9791b4b6499d5a91fe20
Gabriel Tofanello <126465879+gstofanello@users.noreply.github.com>
2023-11-13 11:27:49 +0000

    feat: adicionando allow origin

---
+++ ProjPS2JPA/src/main/java/br/mackenzie/ps2/ExampleJpa/CorsConfig.java
@@ -0,0 +1,10 @@
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+@Override
+    	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    		}
+		}
