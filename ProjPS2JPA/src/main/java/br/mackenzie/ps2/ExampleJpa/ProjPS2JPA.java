695026bf5b004e736205416d7d3be91df8407342
Gabriel Tofanello <126465879+gstofanello@users.noreply.github.com>
2023-11-13 10:55:07 +0000

    feat: adicionando allow origin

--- ProjPS2JPA/src/main/java/br/mackenzie/ps2/ExampleJpa/ProjPS2JPA.java
+++ ProjPS2JPA/src/main/java/br/mackenzie/ps2/ExampleJpa/ProjPS2JPA.java
@@ -10,6 +10,16 @@ public class ProjPS2JPA {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjPS2JPA.class, args);
-	}
 
+		@Configuration
+		public class CorsConfiguration implements WebMvcConfigurer {
+
+		@Override
+    	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    		}
+		}
+	}
 }
